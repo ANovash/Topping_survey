ecf347123c470c89c5d73f2cca44c15e7f212451
ANovash <alexandernovash@gmail.com>
2022-09-26 17:04:50 +0300

    Initial commit

---
+++ src/main/kotlin/org/acc/task/topping/ToppingSurveyApplication.kt
@@ -0,0 +1,11 @@
+package org.acc.task.topping
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToppingSurveyApplication
+
+fun main(args: Array<String>) {
+    runApplication<ToppingSurveyApplication>(*args)
+}
---
+++ src/test/kotlin/org/acc/task/topping/ToppingSurveyApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.acc.task.topping
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToppingSurveyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
